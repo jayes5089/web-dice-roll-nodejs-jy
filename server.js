f96dd57549f2f3ed276096ad9d95849df192d123
jayes5089 <jayes5089@gmail.com>
2024-10-05 20:53:36 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://gray-ocean-05256d010.5.azurestaticapps.net/'
+}));
 
 app.get('/roll-dice', (req, res) => {
     const die1 = Math.floor(Math.random() * 6) + 1;
