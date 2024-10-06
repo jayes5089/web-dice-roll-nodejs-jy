841a24040b487b9e12f7e12a8d3ff8cf09ee923e
jayes5089 <jayes5089@gmail.com>
2024-10-05 20:25:59 -0500

    Added BackEnd Files for web-dice-roll-nodejs-jy

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/roll-dice', (req, res) => {
+    const die1 = Math.floor(Math.random() * 6) + 1;
+    const die2 = Math.floor(Math.random() * 6) + 1;
+
+    res.json({ die1, die2 });
+});
+
+app.get('/roll-dice-fail', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({ message: 'CORS should fail here!' });
+});
+
+app.get('/api/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log('Server running on port 3000...');
+});
